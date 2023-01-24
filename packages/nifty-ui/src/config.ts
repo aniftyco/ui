d86118db5fa58c9d068f263b4cf4a429dda8ba2f
Josh Manders <josh@joshmanders.com>
2023-01-24 14:20:00 -0600

    add support for monorepo and inttial ui package

---
+++ packages/nifty-ui/src/config.ts
@@ -0,0 +1,3 @@
+import { Config } from 'tailwindcss';
+
+export const config: Partial<Config> = {};
---
+++ packages/nifty-ui/src/index.ts
@@ -0,0 +1,5 @@
+import define from 'tailwindcss/plugin';
+import { config } from './config';
+import { plugin } from './plugin';
+
+export default define(plugin, config);
