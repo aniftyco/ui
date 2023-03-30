fa18f5f778a584a7eb8365b80db6bae5d605e862
UsefulBroom <dschmelzer8716@gmail.com>
2023-03-30 13:37:56 -0500

    Checkbox is back on track
    After some help from @joshmanders, Checkboxes are now working as intended, wrapping up some styling.

--- packages/nifty-ui/src/components/Checkbox/index.tsx
+++ packages/nifty-ui/src/components/Checkbox/index.tsx
@@ -1,30 +1,38 @@
-import { ElementRef, forwardRef, useState } from 'react';
+import { forwardRef } from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { IconCheck } from '@tabler/icons-react';
 import { cx } from '../../utils';
 
 export type Props = {
   className?: string;
   size?: 'sm' | 'md' | 'lg' | 'xl';
-  isChecked?: boolean;
+  checked?: boolean;
+  label?: string;
 };
 
-export default () => {
-  const [isChecked, setIsChecked] = useState(false);
-};
-
-export const Checkbox = forwardRef<ElementRef<typeof CheckboxPrimitive.Root>>(({ className, size = 'md' }, ref) => {
+export const Checkbox = forwardRef<HTMLDivElement, Props>(({ className, label, size = 'md' }, ref) => {
   return (
-    <CheckboxPrimitive.Root
-      ref={ref}
-      className={cx('inline-flex items-center justify-center align-middle text-primary-light', className, {
-        'w-4 h-4': size === 'sm',
-        'w-6 h-6': size === 'md',
-        'w-8 h-8': size === 'lg',
-        'w-10 h-10': size === 'xl',
-      })}
-      onClick={(e) => {}}
-    >
-      {<input className="w-32 h-32 text-primary" type="checkbox" />}
-    </CheckboxPrimitive.Root>
+    <div className="flex items-center">
+      <CheckboxPrimitive.Root
+        className={cx(
+          'w-4 h-4 border-2 shrink-0 border-emerald-500 focus:outline-none focus:ring-1 focus:ring-emerald-900  disabled:cursor-not-allowed disabled:opacity-50 dark:border-emerald-700 dark:text-emerald-50 dark:focus:ring-emerald-300 dark:focus:ring-offset-emerald-500 rounded ',
+          className,
+          {
+            'w-4 h-4': size === 'sm',
+            'w-6 h-6': size === 'md',
+            'w-8 h-8 border-4 rounded-md': size === 'lg',
+            'w-10 h-10 border-4 rounded-lg': size === 'xl',
+          }
+        )}
+        defaultChecked={true}
+      >
+        <CheckboxPrimitive.Indicator className="flex items-center justify-center">
+          <IconCheck className="w-4 h-4 text-emerald-900 dark:text-emerald-500" />
+        </CheckboxPrimitive.Indicator>
+      </CheckboxPrimitive.Root>
+      {label && (
+        <label className="pl-[15px] text-[15px] leading-none text-slate-900 dark:text-slate-300">{label}</label>
+      )}
+    </div>
   );
 });
