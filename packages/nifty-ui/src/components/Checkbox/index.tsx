263930f6e99a00265143c310201b04fce3d70fbe
UsefulBroom <dschmelzer8716@gmail.com>
2023-04-03 11:29:43 -0500

    Updated Checkboxes
    Updated checkboxes to make them more subtle when unchecked and more pronounced when they are checked.

--- packages/nifty-ui/src/components/Checkbox/index.tsx
+++ packages/nifty-ui/src/components/Checkbox/index.tsx
@@ -15,25 +15,28 @@ export const Checkbox = forwardRef<HTMLDivElement, Props>(({ className, label, s
     <div className="flex items-center">
       <CheckboxPrimitive.Root
         className={cx(
-          'w-4 h-4 border-2 shrink-0 border-slate-500 focus:outline-none focus:ring-1 focus:ring-emerald-900 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-emerald-50 dark:focus:ring-emerald-300 dark:focus:ring-offset-emerald-500 rounded-md',
+          'w-4 h-4 shrink-0 border-emerald-100 focus:outline-none focus:ring-1 focus:ring-emerald-100 disabled:cursor-not-allowed disabled:opacity-50 dark:text-emerald-50 dark:focus:ring-emerald-300 dark:focus:ring-offset-emerald-500 rounded-md shadow-inner shadow-emerald-400 dark:shadow-emerald-100',
           className,
           {
             'w-4 h-4': size === 'sm',
-            'w-6 h-6 border-2': size === 'md',
-            'w-8 h-8 border-2 rounded-lg': size === 'lg',
-            'w-10 h-10 border-4 rounded-lg': size === 'xl',
+            'w-6 h-6': size === 'md',
+            'w-8 h-8 rounded-lg': size === 'lg',
+            'w-10 h-10 border-2 border-opacity-50 rounded-lg': size === 'xl',
           }
         )}
         defaultChecked={true}
       >
         <CheckboxPrimitive.Indicator className="flex items-center justify-center">
           <IconCheck
-            className={cx('w-2 h-2 bg-fit rounded text-emerald-900 dark:text-emerald-100', {
-              'w-3 h-3 ': size === 'sm',
-              'w-4 h-4': size === 'md',
-              'w-6 h-6': size === 'lg',
-              'w-8 h-8': size === 'xl',
-            })}
+            className={cx(
+              'w-2 h-2 bg-fit bg-emerald-500 rounded text-emerald-900 dark:text-emerald-100 shadow shadow-emerald-200',
+              {
+                'w-3 h-3 ': size === 'sm',
+                'w-4 h-4': size === 'md',
+                'w-6 h-6': size === 'lg',
+                'w-8 h-8': size === 'xl',
+              }
+            )}
           />
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
