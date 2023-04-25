070548aaee6948e53775712190a6bf7641128a0d
UsefulBroom <dschmelzer8716@gmail.com>
2023-04-25 10:51:45 -0500

    Finalized Progress bar

--- packages/nifty-ui/src/components/Progress/index.tsx
+++ packages/nifty-ui/src/components/Progress/index.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
 
 export type Props = {
-  position: number;
+  value: number;
   className?: string;
 };
 
-export const Progress = () => {
-  const [position, setPosition] = React.useState(50);
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => setPosition(66), 500);
-    return () => clearTimeout(timer);
-  }, []);
-
+export const Progress: FC<Props> = ({ value }) => {
   return (
     <ProgressPrimitive.Root
-      className="bg-primary-light dark:bg-primary-dark rounded-xl overflow-hidden w-100% h-6"
-      value={position}
+      className="bg-primary-light relative dark:bg-primary-dark rounded-xl overflow-hidden w-100% h-6"
+      value={value}
+      style={{ transform: 'translateZ(0)' }}
     >
       <ProgressPrimitive.Indicator
-        className="bg-primary-dark dark:bg-primary-light h-100% w-100%"
-        style={{ transform: 'translateX(-${100 - position}%' }}
+        className="bg-primary-dark dark:bg-primary-light w-full h-full rounded-full"
+        style={{ transform: `translateX(-${100 - value}%` }}
       />
     </ProgressPrimitive.Root>
   );
