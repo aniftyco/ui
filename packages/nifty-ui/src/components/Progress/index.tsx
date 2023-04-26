8a24490d42b441f69bed8e7838a482de94c8f607
Daniel Schmelzer <102495884+UsefulBroom@users.noreply.github.com>
2023-04-26 09:33:43 -0500

    Component/progress (#15)
    * Baseline
    
    Got the base set up for the Progress bar, but it is still not doing what i am asking. Quicksave.
    
    * Finalized Progress bar
    
    * Made Changes
    
    Removed divs in MDX,
    index requested changes
    
    * commit changes suggested
    
    Co-authored-by: Josh Manders <josh@joshmanders.com>
    
    ---------
    
    Co-authored-by: Josh Manders <josh@joshmanders.com>

---
+++ packages/nifty-ui/src/components/Progress/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import * as ProgressPrimitive from '@radix-ui/react-progress';
+import { cx } from '../../utils';
+
+export type Props = {
+  value: number;
+  className?: string;
+};
+// transform translate-z-0
+export const Progress: FC<Props> = ({ value, className }) => {
+  return (
+    <ProgressPrimitive.Root
+      className={cx(
+        'bg-primary-light relative dark:bg-primary-dark rounded-xl overflow-hidden w-100% h-6 transform translate-z-0',
+        className
+      )}
+      value={value}
+    >
+      <ProgressPrimitive.Indicator
+        className="bg-primary-dark dark:bg-primary-light w-full h-full rounded-full"
+        style={{ transform: `translateX(-${100 - value}%` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+};
