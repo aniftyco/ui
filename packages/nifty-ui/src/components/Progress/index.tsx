1ddcfb0e9a69993c47e09ec54386fe62b37fc670
UsefulBroom <dschmelzer8716@gmail.com>
2023-04-26 09:29:39 -0500

    Made Changes
    Removed divs in MDX,
    index requested changes

--- packages/nifty-ui/src/components/Progress/index.tsx
+++ packages/nifty-ui/src/components/Progress/index.tsx
@@ -1,20 +1,23 @@
 import React, { FC } from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
+import { cx } from '../../utils';
 
 export type Props = {
   value: number;
   className?: string;
 };
-
-export const Progress: FC<Props> = ({ value }) => {
+// transform translate-z-0
+export const Progress: FC<Props> = ({ value, className }) => {
   return (
     <ProgressPrimitive.Root
-      className="bg-primary-light relative dark:bg-primary-dark rounded-xl overflow-hidden w-100% h-6"
+      className={cx(
+        'bg-primary-light relative dark:bg-primary-dark rounded-xl overflow-hidden w-100% h-6 transform translate-z-0',
+        className
+      )}
       value={value}
-      style={{ transform: 'translateZ(0)' }}
     >
       <ProgressPrimitive.Indicator
-        className="bg-primary-dark dark:bg-primary-light w-full h-full rounded-full"
+        className={`bg-primary-dark dark:bg-primary-light w-full h-full rounded-full`}
         style={{ transform: `translateX(-${100 - value}%` }}
       />
     </ProgressPrimitive.Root>
