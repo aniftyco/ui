410f5db434dd1e84b4b6dca45c228290a9f0b05a
UsefulBroom <dschmelzer8716@gmail.com>
2023-06-26 15:07:06 -0500

    Alert Dialog, Button, Avatar
    Alert Dialog needs the Button, the button needs assistance, Avatar has an error but its almost fixed, MD error

--- packages/nifty-ui/src/components/Avatar/index.tsx
+++ packages/nifty-ui/src/components/Avatar/index.tsx
@@ -1,71 +1,37 @@
-import { ElementRef, forwardRef } from 'react';
+import * as React from 'react';
 import * as AvatarPrimitive from '@radix-ui/react-avatar';
-import md5 from 'md5';
 import { cx } from '../../utils';
 
-import type { RequireAtLeastOne } from 'type-fest';
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cx('relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full', className)}
+    {...props}
+  />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
 
-export type Props = {
-  name?: string;
-  src?: string;
-  hash?: string;
-  email?: string;
-  delayMs?: number;
-  size?: 'sm' | 'md' | 'lg' | 'xl';
-  className?: string;
-};
+const AvatarImage = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image ref={ref} className={cx('aspect-square h-full w-full', className)} {...props} />
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
 
-const getAvatarSrc = (url: string, hash: string, email: string) => {
-  if (url) {
-    return url;
-  }
+const AvatarFallback = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cx('flex h-full w-full items-center justify-center rounded-full bg-muted', className)}
+    {...props}
+  />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
 
-  if (hash) {
-    return `https://www.gravatar.com/avatar/${hash}`;
-  }
-
-  if (email) {
-    return `https://www.gravatar.com/avatar/${md5(email)}`;
-  }
-
-  return null;
-};
-
-export const Avatar = forwardRef<
-  ElementRef<typeof AvatarPrimitive.Root>,
-  RequireAtLeastOne<Props, 'name' | 'src' | 'hash' | 'email'>
->(({ name, hash, email, className, src: url, delayMs = 0, size = 'lg' }, ref) => {
-  const src = getAvatarSrc(url, hash, email);
-
-  return (
-    <AvatarPrimitive.Root
-      ref={ref}
-      className={cx(
-        'inline-flex items-center justify-center overflow-hidden align-middle rounded-full select-none',
-        className,
-        {
-          'w-8 h-8': size === 'sm',
-          'w-10 h-10': size === 'md',
-          'w-12 h-12': size === 'lg',
-          'w-14 h-14': size === 'xl',
-        }
-      )}
-    >
-      {src && <AvatarPrimitive.Image src={src} alt={name} className="h-full w-full rounded-[inherit] object-cover" />}
-      <AvatarPrimitive.Fallback
-        className={cx(
-          'flex items-center justify-center leading-1 h-full w-full rounded-[inherit] text-primary-light bg-primary-dark dark:text-primary-dark dark:bg-primary-light',
-          {
-            'text-xs': size === 'sm',
-            'text-sm': size === 'md',
-            'text-base': size === 'lg',
-            'text-lg': size === 'xl',
-          }
-        )}
-        delayMs={delayMs}
-      >
-        {name.match(/\b(\w)/g).join('')}
-      </AvatarPrimitive.Fallback>
-    </AvatarPrimitive.Root>
-  );
-});
+export { Avatar, AvatarImage, AvatarFallback };
--- packages/nifty-ui/src/index.ts
+++ packages/nifty-ui/src/index.ts
@@ -6,4 +6,5 @@ export { DropdownMenu } from './components/DropdownMenu';
 export { Checkbox } from './components/Checkbox';
 export { Switch } from './components/Switch';
 export { Progress } from './components/Progress';
+export { AlertDialog } from './components/Alert-Dialog';
 export * from './components/Accordion';
