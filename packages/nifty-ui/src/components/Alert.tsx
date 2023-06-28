370e58d9ddfc61c53283a9272b682d01916cfdff
UsefulBroom <dschmelzer8716@gmail.com>
2023-06-28 12:30:36 -0500

    alert

--- packages/nifty-ui/src/components/Alert.tsx
+++ packages/nifty-ui/src/components/Alert.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { cva, VariantProps } from 'class-variance-authority';
 import { cx } from '../utils';
+
+export type Props = {
+  variant? = 'string';
+};
 
 const alertVariants = cva(
   'relative w-full rounded-lg border p-4 [&:has(svg)]:pl-11 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
@@ -17,12 +20,11 @@ const alertVariants = cva(
   }
 );
 
-const Alert = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
->(({ className, variant, ...props }, ref) => (
-  <div ref={ref} role="alert" className={cx(alertVariants({ variant }), className)} {...props} />
-));
+const Alert = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, variant, ...props }, ref) => (
+    <div ref={ref} role="alert" className={cx(alertVariants({ variant }), className)} {...props} />
+  )
+);
 Alert.displayName = 'Alert';
 
 const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
