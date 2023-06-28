407ac386b11bd68e9f413692fa466e5466ed4af0
UsefulBroom <dschmelzer8716@gmail.com>
2023-06-28 12:07:13 -0500

    Progress

--- packages/nifty-ui/src/components/Button.tsx
+++ packages/nifty-ui/src/components/Button.tsx
@@ -2,24 +2,6 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cx } from '../utils';
 
-// const buttonVariants = cva(
-//   'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-//   {
-//
-//       size: {
-//         default: 'h-10 px-4 py-2',
-//         sm: 'h-9 rounded-md px-3',
-//         lg: 'h-11 rounded-md px-8',
-//         icon: 'h-10 w-10',
-//       },
-//     },
-//     defaultVariants: {
-//       variant: 'default',
-//       size: 'default',
-//     },
-//   }
-// );
-
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean;
   variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
@@ -41,7 +23,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             'bg-secondary text-secondary-foreground hover:bg-secondary/80': variant === 'secondary',
             'hover:bg-accent hover:text-accent-foreground': variant === 'ghost',
             'text-primary underline-offset-4 hover:underline': variant === 'link',
-            // 'h-10 px-4 py-2': size === 'default',
+            'h-10 px-4 py-2': size === 'default',
           },
           {}
         )}
@@ -53,4 +35,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = 'Button';
 
-export { Button, buttonVariants };
+export { Button };
--- packages/nifty-ui/src/components/Progress.tsx
+++ packages/nifty-ui/src/components/Progress.tsx
@@ -1,25 +1,27 @@
-import React, { FC } from 'react';
+import React from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
 import { cx } from '../utils';
 
-export type ProgressProps = {
-  value: number;
-  className?: string;
-};
-// transform translate-z-0
-export const Progress: FC<ProgressProps> = ({ value, className }) => {
-  return (
-    <ProgressPrimitive.Root
-      className={cx(
-        'bg-primary-light relative dark:bg-primary-dark rounded-xl overflow-hidden w-100% h-6 transform translate-z-0',
-        className
-      )}
-      value={value}
-    >
-      <ProgressPrimitive.Indicator
-        className="bg-primary-dark dark:bg-primary-light w-full h-full rounded-full"
-        style={{ transform: `translateX(-${100 - value}%` }}
-      />
-    </ProgressPrimitive.Root>
-  );
-};
+// export type ProgressProps = {
+//   value: number;
+//   className?: string;
+// };
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cx('relative h-4 w-full overflow-hidden rounded-full bg-secondary', className)}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-primary transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
