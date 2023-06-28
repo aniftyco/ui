3a12aad5bedfb73b644489be7462809904f70fac
Josh Manders <josh@joshmanders.com>
2023-06-28 11:17:20 -0500

    fix variant issue

--- packages/nifty-ui/src/components/Button.tsx
+++ packages/nifty-ui/src/components/Button.tsx
@@ -22,10 +22,12 @@ import { cx } from '../utils';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean;
+  variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
+  size?: 'default' | 'sm' | 'lg' | 'icon';
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
