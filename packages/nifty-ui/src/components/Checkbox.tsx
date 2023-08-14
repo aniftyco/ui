8c8f8b727f80f03986563ad9c6daf61fe6c235c3
UsefulBroom <dschmelzer8716@gmail.com>
2023-08-14 12:00:27 -0500

    Checkbox
    playing with a few concepts testing the waters, got a solid template. on to the rest.

--- packages/nifty-ui/src/components/Checkbox.tsx
+++ packages/nifty-ui/src/components/Checkbox.tsx
@@ -3,15 +3,26 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { IconCheck } from '@tabler/icons-react';
 import { cx } from '../utils';
 
+export type CheckboxProps = {
+  size: 'sm' | 'lg' | 'xl' | 'default';
+  className?: string;
+};
+
 const Checkbox = forwardRef<
   ElementRef<typeof CheckboxPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+  ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & CheckboxProps
+>(({ className, size, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cx(
       'peer h-4 w-4 shrink-0 rounded-sm border ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-white',
-      className
+      className,
+      {
+        'h-4 w-4': size === 'sm',
+        'h-6 w-6': size === 'default',
+        'h-8 w-8': size === 'lg',
+        'h-10 w-10': size === 'xl',
+      }
     )}
     {...props}
   >
