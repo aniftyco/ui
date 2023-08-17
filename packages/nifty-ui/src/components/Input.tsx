b1e2ec9c6596bf57e7ed55d316b65c017cbbabeb
UsefulBroom <dschmelzer8716@gmail.com>
2023-08-16 22:55:46 -0500

    a few things

--- packages/nifty-ui/src/components/Input.tsx
+++ packages/nifty-ui/src/components/Input.tsx
@@ -2,18 +2,24 @@ import { forwardRef, InputHTMLAttributes } from 'react';
 import { cx } from '../utils';
 
 export type InputProps = {
-  type?: string;
+  type?: 'string';
+  size?: 'sm' | 'md' | 'lg';
   className?: string;
 };
 
-const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, type, ...props }, ref) => {
+const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement> & InputProps>(
+  ({ className, type, size, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cx(
           'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className
+          className,
+          {
+            'h-3': size === 'sm',
+            'h-6 text-md': size === 'md',
+            'h-8 text-lg': size === 'lg',
+          }
         )}
         ref={ref}
         {...props}
--- packages/nifty-ui/src/components/Label.tsx
+++ packages/nifty-ui/src/components/Label.tsx
@@ -2,15 +2,24 @@ import * as React from 'react';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { cx } from '../utils';
 
+export type LabelProps = {
+  size?: 'sm' | 'md' | 'lg';
+};
+
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & LabelProps
+>(({ className, size, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
     className={cx(
       'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-      className
+      className,
+      {
+        'text-sm font-thin': size === 'sm',
+        'text-md font-medium': size === 'md',
+        'text-lg font-semibold': size === 'lg',
+      }
     )}
     {...props}
   />
